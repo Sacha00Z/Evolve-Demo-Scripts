80d9bf7ba4fa4b7fd51b6cec6cb04b0d84686da3
Sacha00Z <sachat@gmail.com>
2023-08-02 09:58:32 +1000

    Working version

--- Call-GetOutputFile.ps1
+++ Call-GetOutputFile.ps1
@@ -1,5 +1,5 @@
 <#
-This script calls the Evolve Generate Create Working Folder API.
+This script calls the Azure Blob Storage API to download a file.
 You need to have a valid settings file and data file for this script to work
 This script is tested in PowerShell Core 7
 #>
@@ -9,18 +9,18 @@ $configFile = ".\settings.json"
 $conf = Get-Content $configFile | ConvertFrom-Json
 
 ## initialise http request
-$uri = $conf.blob.sasUriRoot + "/batch/" + $conf.var.latestWorkingFolderId + "/" + $conf.local.inputFilename + $conf.blob.sasUriAuth
+$uri = $conf.blob.sasUriRoot + "/batch/" + $conf.var.latestWorkingFolderId + "/" + $conf.local.outputFilename + $conf.blob.sasUriAuth
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Content-Type", "application/json")
 $headers.Add("Accept", "application/json")
 $headers.Add("x-ms-blob-type", "BlockBlob")
 
-$body = Get-Content ("./data/" + $conf.local.inputFileName)
-
 ## Execute http request
-$response = Invoke-WebRequest $uri -Method 'PUT' -Headers $headers -Body $body
+$response = Invoke-WebRequest $uri -Method 'GET' -Headers $headers -OutFile ($ENV:Temp + "\" + $conf.local.outputFilename)
 Write-Output $response | Format-List -Property StatusCode, StatusDescription
+
+Start-Process ($ENV:Temp + "\" + $conf.local.outputFilename)
 
 # Pause
 #Read-Host "Press Enter to continue..."
